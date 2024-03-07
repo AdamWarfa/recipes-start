437abb6da687e0a7ab621419ce7220a9d278a4e7
AdamWarfa <Adamwarfadk@gmail.com>
2024-03-08 00:00:29 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Categories } from "./recipes/Categories";
 import Recipe from "./recipes/Recipe";
 import RecipeForm from "./recipes/RecipeForm";
