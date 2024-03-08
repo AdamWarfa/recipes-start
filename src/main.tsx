3adce01d0119544b265e00395a7024992fa3d124
AdamWarfa <Adamwarfadk@gmail.com>
2024-03-08 01:04:12 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import AuthProvider from "./security/AuthProvider";
 
@@ -10,10 +10,10 @@ import AuthProvider from "./security/AuthProvider";
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/recipes-start">
+    <HashRouter basename="/recipes-start">
       <AuthProvider>
         <App />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
